d224df76a8faf0cb0474438ff8c2ed31afece2fa
Kazuki Ota <kaota@microsoft.com>
2024-10-24 09:28:21 +0900

    プロジェクト ファイルを追加します。

---
+++ SyncOverAsyncBlazorApp/Components/PageBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SyncOverAsyncBlazorApp.Components;
+
+public class PageBase : ComponentBase, IHandleEvent
+{
+    Task IHandleEvent.HandleEventAsync(EventCallbackWorkItem item, object? arg)
+    {
+        // イベントハンドラが同期処理である想定なので Task.Run .でラップする
+        var task = Task.Run(() => item.InvokeAsync(arg));
+        StateHasChanged();
+
+        return task.IsCompleted ?
+            Task.CompletedTask :
+            HandleUncompletedEventAsync(task);
+    }
+
+    private async Task HandleUncompletedEventAsync(Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch
+        {
+            if (task.IsCanceled) return;
+            
+            throw;
+        }
+
+        StateHasChanged();
+    }
+}
